e12b1a388a23b2e853019d9679e08cd196041f4c
Artem Minin <artemminin.03@mail.ru>
2025-08-07 21:18:28 +0300

    complete

---
+++ 3/src/app.ts
@@ -0,0 +1,215 @@
+// 1. У вас есть массив объектов вида приведенного в приложении. Необходимо
+// вывести все товары, количество которых больше 10
+// interface Iobj {
+//   id: number;
+//   title: string;
+//   count: number;
+//   price: number;
+// }
+// const obj: Iobj[] = [
+//   { id: 1, title: "Часы", count: 10, price: 500 },
+//   { id: 2, title: "Смартфон", count: 33, price: 1500 },
+//   { id: 3, title: "Моноблок", count: 6, price: 2200 },
+//   { id: 4, title: "Ноутбук", count: 13, price: 3000 },
+//   { id: 5, title: "Планшет", count: 22, price: 2100 },
+// ];
+
+// const res = obj.filter((el: Iobj) => (el.count > 10 ? el : null));
+// console.log(res);
+
+// 2. У вас есть массив объектов вида приведенного в приложении. Необходимо
+// вывести все товары в обратном порядке
+
+// interface Iobj {
+//   id: number;
+//   title: string;
+//   count: number;
+//   price: number;
+// }
+// const obj: Iobj[] = [
+//   { id: 1, title: "Часы", count: 10, price: 500 },
+//   { id: 2, title: "Смартфон", count: 33, price: 1500 },
+//   { id: 3, title: "Моноблок", count: 6, price: 2200 },
+//   { id: 4, title: "Ноутбук", count: 13, price: 3000 },
+//   { id: 5, title: "Планшет", count: 22, price: 2100 },
+// ];
+
+// const res = obj.reverse();
+// console.log(res);
+
+// 3. У вас есть массив объектов вида приведенного в приложении. Необходимо
+// вывести только те товары из положения, количество которых внутри массива
+// кратно 3
+
+// interface Iobj {
+//   id: number;
+//   title: string;
+//   count: number;
+//   price: number;
+// }
+// const obj: Iobj[] = [
+//   { id: 1, title: "Часы", count: 10, price: 500 },
+//   { id: 2, title: "Смартфон", count: 33, price: 1500 },
+//   { id: 3, title: "Моноблок", count: 6, price: 2200 },
+//   { id: 4, title: "Ноутбук", count: 13, price: 3000 },
+//   { id: 5, title: "Планшет", count: 22, price: 2100 },
+// ];
+
+// const res: Iobj[] = obj.filter((el: Iobj) => el.count % 3 === 0);
+// console.log(res);
+
+// 4. У вас есть массив объектов вида приведенного в приложении. Необходимо
+// вывести итоговую стоимость на складе.
+// Итоговая стоимость = количество * цена + ...
+
+// interface Iobj {
+//   id: number;
+//   title: string;
+//   count: number;
+//   price: number;
+// }
+// const obj: Iobj[] = [
+//   { id: 1, title: "Часы", count: 10, price: 500 },
+//   { id: 2, title: "Смартфон", count: 33, price: 1500 },
+//   { id: 3, title: "Моноблок", count: 6, price: 2200 },
+//   { id: 4, title: "Ноутбук", count: 13, price: 3000 },
+//   { id: 5, title: "Планшет", count: 22, price: 2100 },
+// ];
+
+// const res: number = obj.reduce((sum: number, elem: Iobj) => (sum += elem.count * elem.price), 0);
+// console.log(res);
+
+// 5. У вас есть массив объектов вида приведенного в приложении. Необходимо
+// вывести товар с максимальным прайсом
+
+// interface Iobj {
+//   id: number;
+//   title: string;
+//   count: number;
+//   price: number;
+// }
+// const obj: Iobj[] = [
+//   { id: 1, title: "Часы", count: 10, price: 500 },
+//   { id: 2, title: "Смартфон", count: 33, price: 1500 },
+//   { id: 3, title: "Моноблок", count: 6, price: 2200 },
+//   { id: 4, title: "Ноутбук", count: 13, price: 3000 },
+//   { id: 5, title: "Планшет", count: 22, price: 2100 },
+// ];
+
+// const mostExpensive: Iobj = obj.reduce((max: Iobj, item: Iobj) => {
+//   return item.price > max.price ? item : max;
+// });
+// console.log(mostExpensive);
+
+// 6. У вас есть массив объектов вида приведенного в приложении. Необходимо
+// вывести средний прайс среди всех продуктов
+
+// interface Iobj {
+//   id: number;
+//   title: string;
+//   count: number;
+//   price: number;
+// }
+// const obj: Iobj[] = [
+//   { id: 1, title: "Часы", count: 10, price: 500 },
+//   { id: 2, title: "Смартфон", count: 33, price: 1500 },
+//   { id: 3, title: "Моноблок", count: 6, price: 2200 },
+//   { id: 4, title: "Ноутбук", count: 13, price: 3000 },
+//   { id: 5, title: "Планшет", count: 22, price: 2100 },
+// ];
+
+// const middle: number = obj.reduce((sum: number, el: Iobj) => sum + el.price, 0 / obj.length);
+// console.log(middle);
+
+// 7. У вас есть массив объектов вида приведенного в приложении. Необходимо
+// вывести тот товар, где количество * прайс является наибольшим значением
+
+// interface Iobj {
+//   id: number;
+//   title: string;
+//   count: number;
+//   price: number;
+//   result?: number;
+// }
+// const obj: Iobj[] = [
+//   { id: 1, title: "Часы", count: 10, price: 500 },
+//   { id: 2, title: "Смартфон", count: 33, price: 1500 },
+//   { id: 3, title: "Моноблок", count: 6, price: 2200 },
+//   { id: 4, title: "Ноутбук", count: 13, price: 3000 },
+//   { id: 5, title: "Планшет", count: 22, price: 2100 },
+// ];
+
+// const newArr = obj.map((elem) => ({ ...elem, result: elem.count * elem.price }));
+
+// const maxValue: Iobj = newArr.reduce((max, el) => (el.result > max.result ? el : max));
+// console.log(maxValue);
+
+// 8. У вас есть массив строковых значений почт. Необходимо вывести значения
+// массива без дубликатов. Добавить проверки на тип данных, почту
+
+// const mail: string[] = [
+//   "nikita_123@mail.ru",
+//   "hanna_123@mail.ru",
+//   "artem_123@mail.ru",
+//   "sergey_123@mail.ru",
+//   "nikita_123@mail.ru",
+//   "artem_123@mail.ru",
+//   "darya_123@mail.ru",
+// ];
+
+// function checkPost(): void | string[] {
+//   const newArray: string[] = [];
+//   try {
+//     for (let i: number = 0; i < mail.length; i++) {
+//       if (typeof mail[i] !== "string") {
+//         throw new Error("error type");
+//       }
+//       if (!/[A-Z,a-z,0-9\.\-\_\@]+/gm.test(mail[i])) {
+//         throw new Error("error post");
+//       }
+//       if (!newArray.includes(mail[i])) {
+//         newArray.push(mail[i]);
+//       }
+//       return newArray;
+//     }
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+// console.log(checkPost());
+
+//9. Напишите код, который сделает из массива объект
+// [ {name: 'width', value: 10}, {name: 'height', value: 20} ] -> {width: 10, height: 20}
+
+// const obj = [
+//   { name: "width", value: 10 },
+//   { name: "height", value: 20 },
+// ];
+// interface Iobj {
+//   name: string;
+//   value: number;
+// }
+// function reverse(obj: Iobj[]): void {
+//   const res = obj.reduce((ac, elem) => {
+//     ac[elem.name] = elem.value;
+//     return ac;
+//   }, {});
+//   console.log(res);
+// }
+// reverse(obj);
+
+// 10. Реализовать функцию f: при вызове f(2, 3) -> вернет 5, при вызове f(2)(3), тоже
+// вернет 5. Использовать каррирование
+
+// function fun(a: number, b?: number): number | ((b: number) => number) {
+//   if (a && b) return a + b;
+//   else {
+//     return function (b: number): number {
+//       return a + b;
+//     };
+//   }
+// }
+// const res1 = fun(1, 2);
+// const res2 = (fun(1) as any)(2);
+// console.log(res1);
+// console.log(res2);
