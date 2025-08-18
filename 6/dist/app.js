df59a30e7070ab1c8551763491c0d9835fef0c1e
Artem Minin <artemminin.03@mail.ru>
2025-08-18 20:20:20 +0300

    complete

--- 6/dist/app.js
+++ 6/dist/app.js
@@ -5,29 +5,35 @@
 // Входные: Rectangle(5, 4) → Результат: 20
 // Входные: Rectangle(10, 3) → Результат: 30
 // Входные: Rectangle(7, 7) → Результат: 49
-class University {
-    students;
-    constructor(students) {
-        this.students = students;
+class TextAnalyzer {
+    str;
+    constructor(str) {
+        this.str = str;
     }
-}
-class UniversityStudent extends University {
-    getStudentById(id) {
-        for (const student of this.students) {
-            if (student.id === id) {
-                return student;
+    getUniqueWords() {
+        const spl = this.str.split(" ");
+        let arr = [];
+        for (let i = 0; i < spl.length; i++) {
+            if (!arr.includes(spl[i])) {
+                arr.push(spl[i]);
             }
         }
+        return arr.sort();
+    }
+    getWordFrequency() {
+        const spl = this.str.split(" ");
+        let object = {};
+        for (let i = 0; i < spl.length; i++) {
+            if (object.hasOwnProperty(spl[i])) {
+                object[spl[i]] += 1;
+            }
+            else {
+                object[spl[i]] = 1;
+            }
+        }
+        console.log(object);
     }
 }
-class Student {
-    id;
-    name;
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-}
-const students = [new Student(1, "Alice"), new Student(2, "Bob"), new Student(3, "Charlie")];
-const university = new UniversityStudent(students);
-console.log(university.getStudentById(1));
+const str = new TextAnalyzer("ahis generate v strings random text text text strings generate.");
+console.log(str.getUniqueWords());
+console.log(str.getWordFrequency());
--- 6/src/app.ts
+++ 6/src/app.ts
@@ -370,33 +370,243 @@
 // university.getStudentById(1);
 // → Результат: { id: 1, name: 'Alice' }
 
-interface IStudent {
-  id: number;
-  name: string;
-}
+// interface IStudent {
+//   id: number;
+//   name: string;
+// }
 
-abstract class University {
-  protected students: IStudent[];
-  constructor(students: IStudent[]) {
-    this.students = students;
-  }
-  abstract getStudentById(id: number): IStudent;
-}
+// abstract class University {
+//   protected students: IStudent[];
+//   constructor(students: IStudent[]) {
+//     this.students = students;
+//   }
+//   abstract getStudentById(id: number): IStudent;
+// }
 
-class UniversityStudent extends University {
-  getStudentById(id: number): IStudent {
-    for (const student of this.students) {
-      if (student.id === id) {
-        return student;
-      }
-    }
-  }
-}
+// class UniversityStudent extends University {
+//   getStudentById(id: number): IStudent {
+//     for (const student of this.students) {
+//       if (student.id === id) {
+//         return student;
+//       }
+//     }
+//   }
+// }
 
-class Student implements IStudent {
-  constructor(public id: number, public name: string) {}
-}
+// class Student implements IStudent {
+//   constructor(public id: number, public name: string) {}
+// }
 
-const students: IStudent[] = [new Student(1, "Alice"), new Student(2, "Bob"), new Student(3, "Charlie")];
-const university = new UniversityStudent(students);
-console.log(university.getStudentById(1));
+// const students: IStudent[] = [new Student(1, "Alice"), new Student(2, "Bob"), new Student(3, "Charlie")];
+// const university = new UniversityStudent(students);
+// console.log(university.getStudentById(1));
+
+// 11. Создайте класс Counter и интерфейс класса iCounter. У класса должно быть:
+// • Приватное свойство count.
+// • Публичный Метод increment — увеличивает значение счётчика на 1 и возвращает .
+// • Публичный Метод decrement — уменьшает значение счётчика на 1 и возвращает .
+
+// interface iCounter {
+//   increment(): number;
+//   decrement(): number;
+// }
+
+// class Counter implements iCounter {
+//   private count: number = 0;
+//   public increment(): number {
+//     return ++this.count;
+//   }
+//   public decrement(): number {
+//     return --this.count;
+//   }
+// }
+// const count = new Counter();
+// console.log(count.increment());
+// console.log(count.decrement());
+
+// 12. Необходимо реализовать класс QuoteGenerator, добавить интерфейс класса iQuoteGenerator,
+// который содержит список приватных цитат arr и публичный метод getRandomQuote() для
+// получения случайной цитаты из массива.
+
+// interface iQuoteGenerator {
+//   getRandomQuote(): string;
+// }
+
+// class QuoteGenerator implements iQuoteGenerator {
+//   private quotes: string[] = [
+//     "The only limit to our realization of tomorrow is our doubts of today. — Franklin D. Roosevelt",
+//     "Life is 10% what happens to us and 90% how we react to it. — Charles R. Swindoll",
+//     "Success is not final, failure is not fatal: It is the courage to continue that counts. — Winston Churchill",
+//     "Believe you can and you're halfway there. — Theodore Roosevelt",
+//     "Do not watch the clock. Do what it does. Keep going. — Sam Levenson",
+//     "Keep your face always toward the sunshine—and shadows will fall behind you. — Walt Whitman",
+//     "It does not matter how slowly you go as long as you do not stop. — Confucius",
+//     "Your time is limited, don't waste it living someone else's life. — Steve Jobs",
+//     "The best way to predict the future is to create it. — Peter Drucker",
+//     "You miss 100% of the shots you don't take. — Wayne Gretzky",
+//   ];
+//   getRandomQuote() {
+//     const res = Math.floor(Math.random() * (this.quotes.length - 1));
+//     return this.quotes[res];
+//   }
+// }
+// const random = new QuoteGenerator();
+// console.log(random.getRandomQuote());
+
+// 13. Создайте интерфейс INumberStats, который описывает следующие методы:
+// • getEvenNumbers(): number[] — возвращает массив всех чётных чисел.
+// • getAverage(): number — возвращает среднее значение всех чисел.
+// • getPositiveNumbersCount(): number — возвращает количество положительных чисел в
+// массиве.
+// Создайте класс NumberStats, который реализует интерфейс INumberStats.
+// Класс должен принимать массив чисел number[] в конструкторе и реализовывать все методы
+// интерфейса. Также добавьте инкапсуляцию: скрывайте внутренний массив чисел, позволяя
+// работать с ним только через методы класса.
+
+// interface INumberStats {
+//   getEvenNumbers(): number[];
+//   getAverage(): number;
+//   getPositiveNumbersCount(): number;
+// }
+
+// class NumberStats implements INumberStats {
+//   private arr: number[];
+//   constructor(arr: number[]) {
+//     this.arr = arr;
+//   }
+//   getEvenNumbers(): number[] {
+//     return this.arr.filter((n) => n % 2 === 0);
+//   }
+//   getAverage(): number {
+//     return this.arr.reduce((res: number, el: number) => res + el, 0) / this.arr.length;
+//   }
+//   getPositiveNumbersCount(): number {
+//     return this.arr.reduce((acc: number, el: number) => (el > 0 ? acc + 1 : acc), 0);
+//   }
+// }
+// const stats = new NumberStats([1, 2, 3, 4, -1]);
+// console.log(stats.getEvenNumbers());
+// console.log(stats.getAverage());
+// console.log(stats.getPositiveNumbersCount());
+
+// 14. Создайте класс User, содержащий 1 поле массива User[], где каждый объект:
+// • Name = string
+// • Age = number
+// • Gender = 'male' | 'female'
+// Создайте класс UserFilter, наследующийся от User и реализует методы:
+// • filterByAge(min: number, max: number): User[] — возвращает пользователей с возрастом от
+// min до max включительно
+// • filterByGender(gender: 'male' | 'female'): User[] — возвращает пользователей с заданным полом
+
+// interface iUser {
+//   name: string;
+//   age: number;
+//   gender: "male" | "female";
+// }
+
+// class User {
+//   constructor(public user: iUser[]) {}
+// }
+
+// class UserFilter extends User {
+//   filterByAge(min: number, max: number): iUser[] {
+//     return this.user.filter((el: iUser) => el.age > min && el.age < max);
+//   }
+//   filterByGender(gender: "male" | "female") {
+//     return this.user.filter((el) => el.gender === gender);
+//   }
+// }
+// const users: iUser[] = [
+//   { name: "Artem", age: 25, gender: "male" },
+//   { name: "Olga", age: 30, gender: "female" },
+//   { name: "Ivan", age: 40, gender: "male" },
+//   { name: "Darya", age: 22, gender: "female" },
+// ];
+
+// const userFilter = new UserFilter(users);
+// console.log(userFilter.filterByAge(25, 35));
+// console.log(userFilter.filterByGender("male"));
+
+// 15. Создайте класс TextAnalyzer и интерфейс класса iTextAnalyzer, который принимает строку в
+// конструкторе.
+// Реализуйте методы:
+// • getUniqueWords() — возвращает массив уникальных слов в заданной строке,
+// отсортированных по алфавиту (метод sort)
+// • getWordFrequency() — возвращает объект, в котором ключ — слово, значение — количество
+// вхождений
+
+// interface iTextAnalyzer {
+//   getUniqueWords(): string[];
+//   getWordFrequency(): void;
+// }
+
+// class TextAnalyzer implements iTextAnalyzer {
+//   private str: string;
+//   constructor(str: string) {
+//     this.str = str;
+//   }
+//   getUniqueWords() {
+//     const spl = this.str.split(" ");
+//     let arr = [];
+//     for (let i = 0; i < spl.length; i++) {
+//       if (!arr.includes(spl[i])) {
+//         arr.push(spl[i]);
+//       }
+//     }
+//     return arr.sort();
+//   }
+//   getWordFrequency(): void {
+//     const spl = this.str.split(" ");
+//     let object = {};
+//     for (let i: number = 0; i < spl.length; i++) {
+//       if (object.hasOwnProperty(spl[i])) {
+//         object[spl[i]] += 1;
+//       } else {
+//         object[spl[i]] = 1;
+//       }
+//     }
+//     console.log(object);
+//   }
+// }
+// const str = new TextAnalyzer("ahis generate v strings random text text text strings generate.");
+// console.log(str.getUniqueWords());
+// str.getWordFrequency();
+
+// 16. Создайте интерфейс IChecker, описывающий методы:
+// • hasNumbers(): boolean — строка содержит хотя бы одну цифру
+// • hasUpperCase(): boolean — строка содержит хотя бы одну заглавную букву
+// • hasSymbols(): boolean — строка содержит хотя бы один специальный символ (!@#$%^&*())
+// Создайте класс StringChecker, который реализует интерфейс и принимает строку в
+// конструкторе.
+// Каждый метод должен использовать регулярные выражения для выполнения проверок.
+
+// interface IChecker {
+//   str: string;
+//   hasNumbers(): boolean;
+//   hasUpperCase(): boolean;
+//   hasSymbols(): boolean;
+// }
+
+// class StringChecker implements IChecker {
+//   str: string;
+
+//   constructor(str: string) {
+//     this.str = str;
+//   }
+
+//   hasNumbers(): boolean {
+//     return /\d/.test(this.str);
+//   }
+
+//   hasUpperCase(): boolean {
+//     return /[A-ZА-Я]/.test(this.str);
+//   }
+
+//   hasSymbols(): boolean {
+//     return /[!@#$%^&*()]/.test(this.str);
+//   }
+// }
+// const checker = new StringChecker("Hello@2025");
+// console.log(checker.hasNumbers());
+// console.log(checker.hasUpperCase());
+// console.log(checker.hasSymbols());
